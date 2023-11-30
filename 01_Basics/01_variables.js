4032b69da55e24f75a3878a6d5a8c779505e847a
Ayush Tiwari <118131479+ayushtiwariofficial@users.noreply.github.com>
2023-11-30 12:40:36 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const yearOfBirth = 2001;
+let accountEmail = "ayush@gmail.com";
+var password = "123456789";
+randomVariable = "Random";
+
+console.table([yearOfBirth, accountEmail, password, randomVariable]);
